ebec0db216619eedc9a7b80580562acf47f81ea1
britta oblan <brittaoblan@gmail.com>
2024-07-10 23:32:28 +0800

    Update configs

--- src/utils/apollo-client.ts
+++ src/utils/apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: `${process.env.APP_BACKEND_URL}/graphql`
+  uri: `${process.env.APP_BACKEND_URL}`
 });
 
 const authLink = setContext((_, { headers }) => {
