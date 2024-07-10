1b8ab292c1043f6888d00447637d6c10ec1b977f
britta oblan <brittaoblan@gmail.com>
2024-07-10 14:50:47 +0800

    Update env configs

--- src/utils/apollo-client.ts
+++ src/utils/apollo-client.ts
@@ -6,9 +6,7 @@ const client = new ApolloClient({
       watchQuery: { fetchPolicy: "no-cache" },
       query: { fetchPolicy: "no-cache" },
     },
-    link: new HttpLink({
-      uri: `${process.env.NEXT_PUBLIC_APP_BACKEND_URL}/graphql"`
-    }),
+    uri: `${process.env.APP_BACKEND_URL}/graphql`
   });
 
 
