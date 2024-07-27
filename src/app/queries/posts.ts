2618a77fea1817ab844b2b8866b250df85eeaad5
britta oblan <brittaoblan@gmail.com>
2024-07-27 22:54:57 +0800

    Update queries and sorting

--- src/app/queries/posts.ts
+++ src/app/queries/posts.ts
@@ -53,8 +53,30 @@ const getPostCategories = gql`
     }
 `
 
+const getPostsInCategory = gql`
+    query ($category: String!){
+        posts (filters: { Category : { eq: $category } }){
+            data {
+                id
+                attributes {
+                    Title
+                    Published
+                    Content
+                    Category
+                    Summary
+                    Slug
+                    MetaTitle
+                    MetaKeywords
+                    MetaDescription
+                }
+            }
+        }
+    }
+`
+
 export {
     getPostCategories,
+    getPostsInCategory,
     getPostData,
     getAllPost
 }
