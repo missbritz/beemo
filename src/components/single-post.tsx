df9f88c2b5f008459a0c96b2de3b047ae8baf3f0
britta oblan <brittaoblan@gmail.com>
2024-08-04 17:04:56 +0800

    Update buttons and add new refactors and new component chips

--- src/components/single-post.tsx
+++ src/components/single-post.tsx
@@ -1,6 +1,6 @@
 import DateFormatter from "@/utils/date-formatter";
-import Link from "next/link";
 import RichTextBlockRenderer from "./block-renderer";
+import Chips from "./chips";
 
 export default function SinglePost ({ post }: any) {
     return (
@@ -12,7 +12,7 @@ export default function SinglePost ({ post }: any) {
                 <h2 className="text-cpink-900 font-bold text-3xl md:text-5xl my-5">
                 {post.attributes.Title}
                 </h2>
-                <Link href={`/notes/${post.attributes.Category}`} className="relative z-0 rounded-full bg-slate-900 px-3 py-1.5 font-medium text-gray-600  hover:rainbow-bg text-sm">{post.attributes.Category}</Link>
+                <Chips label={post.attributes.Category} link={`/notes/${post.attributes.Category}`} />
                 <p className="text-gray-400 my-3">{post.attributes.Summary}</p>
                 <hr className="my-8 border-b border-solid border-slate-800"/>
                 {post.attributes.Content && <RichTextBlockRenderer content={post.attributes.Content} />}
